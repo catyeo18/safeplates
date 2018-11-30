f96765a898c3902cca7ecf4a492f2b8e5b2ee2ca
Catherine <catherineyeo2018@gmail.com>
2018-11-30 02:20:34 -0500

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+def returnSoup(link):
+	print("Entered function")
+	browser = webdriver.Chrome()
+
+	browser.get(link)
+	browser.implicitly_wait(30)
+
+	# parse the html using beautiful soup and store in variable `soup`
+	soup = BeautifulSoup(browser.page_source, 'html.parser')
+	
+	print(soup)
+	return soup
